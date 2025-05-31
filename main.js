88b7717b82ff0b31e45c642e4712db1d3335e9ff
madhavi-lad <madhavilad05@gmail.com>
2025-05-31 15:59:49 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const libexpress = require("express")
+
+const server = libexpress()
+
+server.post("/user", (req, res)=>{
+    console.log("User Request")
+    res.send("User created")
+})
+
+server.post("/team", (req, res)=>{
+    console.log("Team Request")
+    res.send("Team created")
+})
+
+server.post("/player", (req, res)=>{
+    console.log("Player Request")
+    res.send("Player created")
+})
+
+server.listen(8000,()=>{
+    console.log("Server is listening on port 8000")
+})
