cac10d068d6bbf9ff891f41ac6ff21f4833068de
madhavi-lad <madhavilad05@gmail.com>
2025-06-03 15:03:06 +0530

    db connection

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
+const {MongoClient} = require("mongodb")
 const libExpress = require("express")
 const cors = require("cors")
 const server = libExpress()
 server.use(cors())
 
+const connection = new MongoClient("mongodb://Madhavi:madhavi05@localhost:27017/IMS?authSource=IMS")
+
+//user signup
 server.post("/user", (req, res)=>{
     console.log("user request")
-    res.send("user is created")
+
+    if(req.body.name && req.body.email && req.body.password && req.body.phone){
+    connection.connect().
+    then(() => connection.db("IMS")).
+    then((db) => db.collection("USER")).
+    then((collection) => collection.insertOne({
+        name: req.body.name,
+        email: req.body.email,
+        passwoed: req.body.password,
+        phone: req.body.phone
+    })).
+    then(() => res.json({message: "User created successfully"})).
+    catch((error) => res.json({message: "Error creating user ", error}))
+    }
 })
 
-server.get("/user", (req, res)=>{
+//user login
+server.get("/token", (req, res)=>{
     console.log("user request")
-    res.json(
+
+    connection.connect().
+    then(() => connection.db("IMS")). 
+    then((db) => db.collection("USER")). 
+    then((collection) => collection.find().toArray()). 
+    then((result) => console.log(result)). 
+    catch((error) => console.log("Database Connection error. ", error))
+
+
+    /*res.json(
         [   {name: "usr1"},
             {name: "user2"},
             {name: "user3"}
         ]
     )
+    */
 })
 
 server.post("/team", (req, res)=>{
