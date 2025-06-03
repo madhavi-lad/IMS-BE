b237c38b18fa77ff380137e5b82764c19eb0e01e
madhavi-lad <madhavilad05@gmail.com>
2025-06-03 16:56:16 +0530

    insert/find

--- main.js
+++ main.js
@@ -3,13 +3,37 @@ const libExpress = require("express")
 const cors = require("cors")
 const server = libExpress()
 server.use(cors())
+server.use(libExpress.json()) //converts into json format
 
 const connection = new MongoClient("mongodb://Madhavi:madhavi05@localhost:27017/IMS?authSource=IMS")
 
 //user signup
-server.post("/user", (req, res)=>{
-    console.log("user request")
+server.post("/user", async (req, res)=>{
+    //console.log("user request")
 
+    if(req.body.name && req.body.email && req.body.password && req.body.phone){
+        await connection.connect()
+        const db = await connection.db("IMS")
+        const collection = await db.collection("USER")
+        const result = await collection.find({"email": req.body.email}).toArray()
+
+        if(result.length > 0)
+            res.json("User already exist!")
+
+        else{
+            await collection.insertOne({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                phone: req.body.phone
+            })
+            res.json("User created successfully!")
+        }
+    }
+
+
+
+    /*
     if(req.body.name && req.body.email && req.body.password && req.body.phone){
     connection.connect().
     then(() => connection.db("IMS")).
@@ -23,6 +47,7 @@ server.post("/user", (req, res)=>{
     then(() => res.json({message: "User created successfully"})).
     catch((error) => res.json({message: "Error creating user ", error}))
     }
+    */
 })
 
 //user login
