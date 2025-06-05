62c8ae50e631839be5f795aeb0fc9b9b7269bac8
madhavi-lad <madhavilad05@gmail.com>
2025-06-05 16:39:13 +0530

    thursday 5/6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {MongoClient, Admin} = require("mongodb")
+const { MongoClient, Admin, Collection, ObjectId } = require("mongodb")
 const libRandomString = require("randomstring")
 const libExpress = require("express")
 const cors = require("cors")
@@ -9,19 +9,19 @@ server.use(libExpress.json()) //converts into json format
 const connection = new MongoClient("mongodb://Madhavi:madhavi05@localhost:27017/IMS?authSource=IMS")
 
 //user signup
-server.post("/user", async (req, res)=>{
+server.post("/user", async (req, res) => {
     //console.log("user request")
 
-    if(req.body.name && req.body.email && req.body.password && req.body.phone){
+    if (req.body.name && req.body.email && req.body.password && req.body.phone) {
         await connection.connect()
         const db = await connection.db("IMS")
         const collection = await db.collection("USER")
-        const result = await collection.find({"email": req.body.email}).toArray()
+        const result = await collection.find({ "email": req.body.email }).toArray()
 
-        if(result.length > 0)
+        if (result.length > 0)
             res.json("User already exist!")
 
-        else{
+        else {
             await collection.insertOne({
                 name: req.body.name,
                 email: req.body.email,
@@ -30,9 +30,11 @@ server.post("/user", async (req, res)=>{
             })
             res.json("User created successfully!")
         }
+
+        connection.close()
     }
 
-    else{
+    else {
         res.json("Please fill all the fields!")
     }
 
@@ -56,89 +58,137 @@ server.post("/user", async (req, res)=>{
 })
 
 //user login
-server.post("/token", async (req, res)=>{
+server.post("/token", async (req, res) => {
     console.log("user request")
 
-    if(req.body.email && req.body.password){
+    if (req.body.email && req.body.password) {
         await connection.connect()
         const db = await connection.db("IMS")
         const collection = await db.collection("USER")
-        const result = await collection.find({"email": req.body.email, "password": req.body.password}).toArray()
-        
-        if(result.length > 0)
-        {
+        const result = await collection.find({ "email": req.body.email, "password": req.body.password }).toArray()
+
+        if (result.length > 0) {
             // genetare token
             const generatedToken = libRandomString.generate(6)
 
             // register token against user
             const user = result[0]
-            
+
             await collection.updateOne(
                 { _id: user._id },
-                {$set: {token: generatedToken}}
+                { $set: { token: generatedToken } }
             )
 
             // return token to user
-            res.status(200).json({token: generatedToken})
+            res.status(200).json({ token: generatedToken })
         }
-        else{
-            res.status(400).json({message: "Invalid email or password"})
+        else {
+            res.status(400).json({ message: "Invalid email or password" })
         }
+
+        connection.close()
     }
 
-    else{
-        res.status(401).json({message: "All fields are required"})
+    else {
+        res.status(401).json({ message: "All fields are required" })
     }
 
 })
 
 
 // user role
-server.get("/user/role", async (req, res) =>{
+server.get("/user/role", async (req, res) => {
     console.log("user role request")
 
-    if(req.headers.token){
-        
+    if (req.headers.token) {
+
         await connection.connect()
         const db = await connection.db("IMS")
         const collection = await db.collection("USER")
-        const result = await collection.find({"token": req.headers.token}).toArray()
+        const result = await collection.find({ "token": req.headers.token }).toArray()
 
-        if(result.length > 0)
-        {
+        if (result.length > 0) {
             const user = result[0]
 
             const roles = {
                 admin: user.is_admin === true,
-                onwer: !!user.owner_of,
-                player: !!user.playeing_for
+                owner: !!user.owner_of,
+                player: !!user.playing_for
             }
 
             res.status(200).json(roles)
         }
 
-        else{
-            res.status(401).json({message: "Invalid token"})
+        else {
+            res.status(401).json({ message: "Invalid token" })
         }
-        
+
+        connection.close()
+
     }
-    else{
-        res.status(401).json({message: "Unauthorized"})
+    else {
+        res.status(401).json({ message: "Unauthorized" })
     }
 })
 
 
-server.post("/team", (req, res)=>{
+server.get("/players", async (req, res) => {
+
+    await connection.connect()
+    const db = await connection.db("IMS")
+    const collection = await db.collection("USER")
+    const result = await collection.find({ playing_for: { $exists: true } }).toArray()
+
+    res.status(200).json(result)
+
+    connection.close()
+})
+
+server.get("/teams", async (req, res) => {
+
+    await connection.connect()
+    const db = await connection.db("IMS")
+    const collection = await db.collection("TEAM")
+    const result = await collection.find().toArray()
+
+    res.status(200).json(result)
+
+    connection.close()
+})
+
+server.get("/players/:id/stats", async (req, res) => {
+
+    console.log("stats req")
+
+    if (req.params.id) {
+        await connection.connect()
+        const db = await connection.db("IMS")
+        const collection = await db.collection("USER")
+        const result = await collection.findOne({ "_id": new ObjectId(req.params.id)})
+
+        res.status(200).json(result)
+
+        connection.close()
+    }
+
+    else{
+        res.status(400).json({ message: "Bad Request" })
+    }
+
+})
+
+
+server.post("/team", (req, res) => {
     console.log("team request")
     res.send("Team is created")
 })
 
-server.post("/player", (req, res)=>{
+server.post("/player", (req, res) => {
     console.log("player request")
     res.send("Player is created")
 })
 
-server.listen(8000, ()=> {
+server.listen(8000, () => {
     console.log("Server is running on port 8000")
 })
 
